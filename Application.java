71c3ca890ba99b375c7e770af52a3048a5e39283
Хабиров <airkhabirov@yandex.ru>
2016-09-21 09:32:43 +0400

    initTask

---
+++ Application.java
@@ -0,0 +1,10 @@
+package PO43.Khabirov.wdad;
+
+/**
+ * Created by Хабиров on 21.09.2016.
+ */
+public class Application {
+    public static void main(String[]args){
+        System.out.println("I am Airat Khabirov, and I'm not a monkey");
+    }
+}
