a8b12e01bab0033ec500e8122d713b82e7589a66
Хабиров <airkhabirov@yandex.ru>
2016-09-27 23:27:06 +0400

    Added entry point

--- Application.java
+++ Application.java
@@ -2,6 +2,6 @@ package PO43.Khabirov.wdad;
 
 public class Application {
     public static void main(String[]args){
-        System.out.println("I am Airat Khabirov, and I'm not a monkey");
+        System.out.print("I am Airat Khabirov, and I am not monkey");
     }
 }
